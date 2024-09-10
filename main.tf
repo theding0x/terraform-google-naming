c4799ac59ea82dff15e523c16fa96a9ea4de231b
Aaron Hulse <aaron@spaceship-moon.com>
2024-09-09 20:25:48 -0500

    Add compute backend resources

--- main.tf
+++ main.tf
@@ -36,6 +36,96 @@ locals {
   // Names based in the recomendations of
   // https://cloud.google.com/compute/docs/naming-resources
   gcp = {
+    compute_address = {
+      name        = substr(join("-", compact([local.prefix, "address", local.suffix])), 0, 50)
+      name_unique = substr(join("-", compact([local.prefix, "address", local.suffix_unique])), 0, 50)
+      dashes      = true
+      slug        = "address"
+      min_length  = 1
+      max_length  = 50
+      scope       = "project"
+      regex       = "^[a-z]([-a-z0-9]*[a-z0-9])?$"
+    }
+    compute_attached_disk = {
+      name        = substr(join("-", compact([local.prefix, "disk", local.suffix])), 0, 50)
+      name_unique = substr(join("-", compact([local.prefix, "disk", local.suffix_unique])), 0, 50)
+      dashes      = true
+      slug        = "disk"
+      min_length  = 1
+      max_length  = 50
+      scope       = "project"
+      regex       = "^[a-z]([-a-z0-9]*[a-z0-9])?$"
+    }
+    compute_autoscaler = {
+      name        = substr(join("-", compact([local.prefix, "autoscaler", local.suffix])), 0, 50)
+      name_unique = substr(join("-", compact([local.prefix, "autoscaler", local.suffix_unique])), 0, 50)
+      dashes      = true
+      slug        = "autoscaler"
+      min_length  = 1
+      max_length  = 50
+      scope       = "project"
+      regex       = "^[a-z]([-a-z0-9]*[a-z0-9])?$"
+    }
+    compute_backend_bucket = {
+      name        = substr(join("-", compact([local.prefix, "bucket", local.suffix])), 0, 50)
+      name_unique = substr(join("-", compact([local.prefix, "bucket", local.suffix_unique])), 0, 50)
+      dashes      = true
+      slug        = "bucket"
+      min_length  = 1
+      max_length  = 50
+      scope       = "project"
+      regex       = "^[a-z]([-a-z0-9]*[a-z0-9])?$"
+    }
+    compute_backend_bucket_signed_url_key = {
+      name        = substr(join("-", compact([local.prefix, "key", local.suffix])), 0, 50)
+      name_unique = substr(join("-", compact([local.prefix, "key", local.suffix_unique])), 0, 50)
+      dashes      = true
+      slug        = "key"
+      min_length  = 1
+      max_length  = 50
+      scope       = "project"
+      regex       = "^[a-z]([-a-z0-9]*[a-z0-9])?$"
+    }
+    compute_backend_iam = {
+      name        = substr(join("-", compact([local.prefix, "iam", local.suffix])), 0, 50)
+      name_unique = substr(join("-", compact([local.prefix, "iam", local.suffix_unique])), 0, 50)
+      dashes      = true
+      slug        = "iam"
+      min_length  = 1
+      max_length  = 50
+      scope       = "project"
+      regex       = "^[a-z]([-a-z0-9]*[a-z0-9])?$"
+    }
+    compute_backend_service = {
+      name        = substr(join("-", compact([local.prefix, "service", local.suffix])), 0, 50)
+      name_unique = substr(join("-", compact([local.prefix, "service", local.suffix_unique])), 0, 50)
+      dashes      = true
+      slug        = "service"
+      min_length  = 1
+      max_length  = 50
+      scope       = "project"
+      regex       = "^[a-z]([-a-z0-9]*[a-z0-9])?$"
+    }
+    compute_backend_service_iam = {
+      name        = substr(join("-", compact([local.prefix, "iam", local.suffix])), 0, 50)
+      name_unique = substr(join("-", compact([local.prefix, "iam", local.suffix_unique])), 0, 50)
+      dashes      = true
+      slug        = "iam"
+      min_length  = 1
+      max_length  = 50
+      scope       = "project"
+      regex       = "^[a-z]([-a-z0-9]*[a-z0-9])?$"
+    }
+    compute_backend_service_signed_url_key = {
+      name        = substr(join("-", compact([local.prefix, "key", local.suffix])), 0, 50)
+      name_unique = substr(join("-", compact([local.prefix, "key", local.suffix_unique])), 0, 50)
+      dashes      = true
+      slug        = "key"
+      min_length  = 1
+      max_length  = 50
+      scope       = "project"
+      regex       = "^[a-z]([-a-z0-9]*[a-z0-9])?$"
+    }
     compute_ha_vpn_gateway = {
       name        = substr(join("-", compact([local.prefix, "vpn", local.suffix])), 0, 50)
       name_unique = substr(join("-", compact([local.prefix, "vpn", local.suffix_unique])), 0, 50)
@@ -78,6 +168,42 @@ locals {
     }
   }
   validation = {
+    compute_address = {
+      valid_name        = length(regexall(local.gcp.compute_address.regex, local.gcp.compute_address.name)) > 0 && length(local.gcp.compute_address.name) > local.gcp.compute_address.min_length
+      valid_name_unique = length(regexall(local.gcp.compute_address.regex, local.gcp.compute_address.name_unique)) > 0
+    }
+    compute_attached_disk = {
+      valid_name        = length(regexall(local.gcp.compute_attached_disk.regex, local.gcp.compute_attached_disk.name)) > 0 && length(local.gcp.compute_attached_disk.name) > local.gcp.compute_attached_disk.min_length
+      valid_name_unique = length(regexall(local.gcp.compute_attached_disk.regex, local.gcp.compute_attached_disk.name_unique)) > 0
+    }
+    compute_autoscaler = {
+      valid_name        = length(regexall(local.gcp.compute_autoscaler.regex, local.gcp.compute_autoscaler.name)) > 0 && length(local.gcp.compute_autoscaler.name) > local.gcp.compute_autoscaler.min_length
+      valid_name_unique = length(regexall(local.gcp.compute_autoscaler.regex, local.gcp.compute_autoscaler.name_unique)) > 0
+    }
+    compute_backend_bucket = {
+      valid_name        = length(regexall(local.gcp.compute_backend_bucket.regex, local.gcp.compute_backend_bucket.name)) > 0 && length(local.gcp.compute_backend_bucket.name) > local.gcp.compute_backend_bucket.min_length
+      valid_name_unique = length(regexall(local.gcp.compute_backend_bucket.regex, local.gcp.compute_backend_bucket.name_unique)) > 0
+    }
+    compute_backend_bucket_signed_url_key = {
+      valid_name        = length(regexall(local.gcp.compute_backend_bucket_signed_url_key.regex, local.gcp.compute_backend_bucket_signed_url_key.name)) > 0 && length(local.gcp.compute_backend_bucket_signed_url_key.name) > local.gcp.compute_backend_bucket_signed_url_key.min_length
+      valid_name_unique = length(regexall(local.gcp.compute_backend_bucket_signed_url_key.regex, local.gcp.compute_backend_bucket_signed_url_key.name_unique)) > 0
+    }
+    compute_backend_iam = {
+      valid_name        = length(regexall(local.gcp.compute_backend_iam.regex, local.gcp.compute_backend_iam.name)) > 0 && length(local.gcp.compute_backend_iam.name) > local.gcp.compute_backend_iam.min_length
+      valid_name_unique = length(regexall(local.gcp.compute_backend_iam.regex, local.gcp.compute_backend_iam.name_unique)) > 0
+    }
+    compute_backend_service = {
+      valid_name        = length(regexall(local.gcp.compute_backend_service.regex, local.gcp.compute_backend_service.name)) > 0 && length(local.gcp.compute_backend_service.name) > local.gcp.compute_backend_service.min_length
+      valid_name_unique = length(regexall(local.gcp.compute_backend_service.regex, local.gcp.compute_backend_service.name_unique)) > 0
+    }
+    compute_backend_service_iam = {
+      valid_name        = length(regexall(local.gcp.compute_backend_service_iam.regex, local.gcp.compute_backend_service_iam.name)) > 0 && length(local.gcp.compute_backend_service_iam.name) > local.gcp.compute_backend_service_iam.min_length
+      valid_name_unique = length(regexall(local.gcp.compute_backend_service_iam.regex, local.gcp.compute_backend_service_iam.name_unique)) > 0
+    }
+    compute_backend_service_signed_url_key = {
+      valid_name        = length(regexall(local.gcp.compute_backend_service_signed_url_key.regex, local.gcp.compute_backend_service_signed_url_key.name)) > 0 && length(local.gcp.compute_backend_service_signed_url_key.name) > local.gcp.compute_backend_service_signed_url_key.min_length
+      valid_name_unique = length(regexall(local.gcp.compute_backend_service_signed_url_key.regex, local.gcp.compute_backend_service_signed_url_key.name_unique)) > 0
+    }
     compute_ha_vpn_gateway = {
       valid_name        = length(regexall(local.gcp.compute_ha_vpn_gateway.regex, local.gcp.compute_ha_vpn_gateway.name)) > 0 && length(local.gcp.compute_ha_vpn_gateway.name) > local.gcp.compute_ha_vpn_gateway.min_length
       valid_name_unique = length(regexall(local.gcp.compute_ha_vpn_gateway.regex, local.gcp.compute_ha_vpn_gateway.name_unique)) > 0
--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,51 @@ output "unique-seed" {
 output "validation" {
   value = local.validation
 }
+output "compute_address" {
+  value       = local.gcp.compute_address
+  description = "Compute Address"
+}
+
+output "compute_attached_disk" {
+  value       = local.gcp.compute_attached_disk
+  description = "Compute Attached Disk"
+}
+
+output "compute_autoscaler" {
+  value       = local.gcp.compute_autoscaler
+  description = "Compute Autoscaler"
+}
+
+output "compute_backend_bucket" {
+  value       = local.gcp.compute_backend_bucket
+  description = "Compute Backend Bucket"
+}
+
+output "compute_backend_bucket_signed_url_key" {
+  value       = local.gcp.compute_backend_bucket_signed_url_key
+  description = "Compute Backend Bucket Signed Url Key"
+}
+
+output "compute_backend_iam" {
+  value       = local.gcp.compute_backend_iam
+  description = "Compute Backend Iam"
+}
+
+output "compute_backend_service" {
+  value       = local.gcp.compute_backend_service
+  description = "Compute Backend Service"
+}
+
+output "compute_backend_service_iam" {
+  value       = local.gcp.compute_backend_service_iam
+  description = "Compute Backend Service Iam"
+}
+
+output "compute_backend_service_signed_url_key" {
+  value       = local.gcp.compute_backend_service_signed_url_key
+  description = "Compute Backend Service Signed Url Key"
+}
+
 output "compute_ha_vpn_gateway" {
   value       = local.gcp.compute_ha_vpn_gateway
   description = "Compute Ha Vpn Gateway"
